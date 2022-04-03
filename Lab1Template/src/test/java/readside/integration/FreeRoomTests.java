2383e5dc66ecc093e6f2b400ed3cac8dd966f500
Delta <noahjul@gmail.com>
2022-04-03 15:35:23 +0200

    FreeRoomTests for cancellation

--- Lab1Template/src/test/java/readside/integration/FreeRoomTests.java
+++ Lab1Template/src/test/java/readside/integration/FreeRoomTests.java
@@ -1,6 +1,7 @@
 package readside.integration;
 
 
+import eventside.event.BookingCanceledEvent;
 import eventside.event.BookingCreatedEvent;
 
 import org.junit.jupiter.api.Test;
@@ -14,13 +15,14 @@ import readside.projection.IProjection;
 import readside.projection.Projection;
 
 import java.time.LocalDate;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = {Projection.class, FreeRoomRepository.class, BookingRepository.class})
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) // should reset repo after every test
 class FreeRoomTests {
 
     @Autowired
@@ -37,6 +39,10 @@ class FreeRoomTests {
                 LocalDate.of(2022, 3, dayTo),
                 Set.of("S_001"),
                 "Noah");
+    }
+
+    private BookingCanceledEvent canceledEventFromCreateEvent(BookingCreatedEvent e) {
+        return new BookingCanceledEvent(e.getBookingId(), e.getArrivalDate(), e.getDepartureDate(), Map.of("S_001", 1));
     }
 
     // Single-Booking test
@@ -127,6 +133,8 @@ class FreeRoomTests {
 
 
     // Double booking with no time between test
+
+
     @Test
     void given_two_bookings_when_free_rooms_2022_03_02_to_2022_03_05_then_none() {
 
@@ -286,4 +294,94 @@ class FreeRoomTests {
     }
 
 
+    // cancellation tests
+
+
+    @Test
+    void given_only_one_booking_when_cancel_then_one_big_free_space() {
+        var createdEvent = createEvent(10, 15);
+        var canceledEvent = canceledEventFromCreateEvent(createdEvent);
+        projection.processIncomingBookingCreatedEvent(createdEvent);
+        projection.processIncomingBookingCanceledEvent(canceledEvent);
+
+        var from = LocalDate.of(2022, 3, 10);
+        var to = LocalDate.of(2022, 3,15);
+
+        var response = repo.getBetween(from, to).stream().filter(r -> r.getRoomnumber().equals("S_001")).findAny();
+
+        assertTrue(response.isPresent());
+        assertEquals(LocalDate.MIN, response.get().getFrom());
+        assertEquals(LocalDate.MAX, response.get().getTo());
+    }
+
+    @Test
+    void given_two_bookings_next_to_each_other_booking_when_cancel_first_then_first_free_time_expands() {
+        var createdEvent1 = createEvent(10, 15);
+        var createdEvent2 = createEvent(15, 20);
+        var canceledEvent = canceledEventFromCreateEvent(createdEvent1);
+        projection.processIncomingBookingCreatedEvent(createdEvent1);
+        projection.processIncomingBookingCreatedEvent(createdEvent2);
+        projection.processIncomingBookingCanceledEvent(canceledEvent);
+
+        var from = LocalDate.of(2022, 3, 10);
+        var to = LocalDate.of(2022, 3,15);
+
+        var response = repo.getBetween(from, to).stream().filter(r -> r.getRoomnumber().equals("S_001")).findAny();
+
+        assertTrue(response.isPresent());
+        assertEquals(LocalDate.MIN, response.get().getFrom());
+        assertEquals(to, response.get().getTo());
+    }
+
+    @Test
+    void given_two_bookings_next_to_each_other_booking_when_cancel_second_then_second_free_time_expands() {
+        var createdEvent1 = createEvent(10, 15);
+        var createdEvent2 = createEvent(15, 20);
+        var canceledEvent = canceledEventFromCreateEvent(createdEvent2);
+        projection.processIncomingBookingCreatedEvent(createdEvent1);
+        projection.processIncomingBookingCreatedEvent(createdEvent2);
+        projection.processIncomingBookingCanceledEvent(canceledEvent);
+
+        var from = LocalDate.of(2022, 3, 15);
+        var to = LocalDate.of(2022, 3,20);
+
+        var response = repo.getBetween(from, to).stream().filter(r -> r.getRoomnumber().equals("S_001")).findAny();
+
+        assertTrue(response.isPresent());
+        assertEquals(from, response.get().getFrom());
+        assertEquals(LocalDate.MAX, response.get().getTo());
+    }
+
+    @Test
+    void given_three_bookings_next_to_each_other_booking_when_cancel_middle_then_free_space_created() {
+        var createdEvent1 = createEvent(10, 15);
+        var createdEvent2 = createEvent(15, 20);
+        var createdEvent3 = createEvent(20, 25);
+        var canceledEvent = canceledEventFromCreateEvent(createdEvent2);
+        projection.processIncomingBookingCreatedEvent(createdEvent1);
+        projection.processIncomingBookingCreatedEvent(createdEvent2);
+        projection.processIncomingBookingCreatedEvent(createdEvent3);
+        projection.processIncomingBookingCanceledEvent(canceledEvent);
+
+        var from = LocalDate.of(2022, 3, 15);
+        var to = LocalDate.of(2022, 3,20);
+
+        var response = repo.getBetween(from, to).stream().filter(r -> r.getRoomnumber().equals("S_001")).findAny();
+
+        assertTrue(response.isPresent());
+        assertEquals(from, response.get().getFrom());
+        assertEquals(to, response.get().getTo());
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
