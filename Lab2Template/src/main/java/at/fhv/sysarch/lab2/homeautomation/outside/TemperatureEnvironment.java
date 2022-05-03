e3c923e8bdefc553b5af13c44a32211fd17ddec7
BuildTools <unconfigured@null.spigotmc.org>
2022-05-03 08:29:28 +0200

    temperature sensor

--- Lab2Template/src/main/java/at/fhv/sysarch/lab2/homeautomation/outside/TemperatureEnvironment.java
+++ Lab2Template/src/main/java/at/fhv/sysarch/lab2/homeautomation/outside/TemperatureEnvironment.java
@@ -1,17 +1,19 @@
 package at.fhv.sysarch.lab2.homeautomation.outside;
 
+import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.*;
+import at.fhv.sysarch.lab2.homeautomation.devices.TemperatureSensor;
 import at.fhv.sysarch.lab2.homeautomation.shared.Temperature;
 
 import java.time.Duration;
 import java.util.Random;
 
-public class TemperatureEnvironment extends AbstractBehavior<TemperatureEnvironment.TemperatureUpdateCommand> {
+public class TemperatureEnvironment extends AbstractBehavior<TemperatureEnvironment.TemperatureEnvironmentCommand> {
 
-    public interface TemperatureUpdateCommand{}
+    public interface TemperatureEnvironmentCommand {}
 
-    public static final class TemperatureUpdate implements TemperatureUpdateCommand{
+    public static final class TemperatureUpdate implements TemperatureEnvironmentCommand {
         Temperature currentTemp;
 
         public TemperatureUpdate(Temperature currentTemp) {
@@ -19,48 +21,57 @@ public class TemperatureEnvironment extends AbstractBehavior<TemperatureEnvironm
         }
     }
 
-    private Temperature currentTemperature;
-    private final TimerScheduler<TemperatureUpdateCommand> temperatureTimeSchedule;
+    public static final class ReceiveTemperatureRequest implements TemperatureEnvironmentCommand {
+        ActorRef<TemperatureSensor.TemperatureSensorCommand> sensor;
 
-    public static Behavior<TemperatureUpdateCommand> create(Temperature initTemp)  {
+        public ReceiveTemperatureRequest(ActorRef<TemperatureSensor.TemperatureSensorCommand> sensor) {
+            this.sensor = sensor;
+        }
+    }
+
+
+    private Random random = new Random();
+    private Temperature currentTemperature;
+    private final TimerScheduler<TemperatureEnvironmentCommand> temperatureTimeSchedule;
+
+    public static Behavior<TemperatureEnvironmentCommand> create(Temperature initTemp)  {
         return Behaviors.setup(context -> Behaviors.withTimers(timer -> new TemperatureEnvironment(context, initTemp, timer)));
     }
 
-    public TemperatureEnvironment(ActorContext<TemperatureUpdateCommand> context, Temperature initTemp, TimerScheduler<TemperatureUpdateCommand> temperatureTimeSchedule) {
+    public TemperatureEnvironment(ActorContext<TemperatureEnvironmentCommand> context, Temperature initTemp, TimerScheduler<TemperatureEnvironmentCommand> temperatureTimeSchedule) {
         super(context);
         this.currentTemperature = initTemp;
         this.temperatureTimeSchedule = temperatureTimeSchedule;
-        this.temperatureTimeSchedule.startTimerAtFixedRate(new TemperatureUpdate(currentTemperature), Duration.ofSeconds(5));
+        this.temperatureTimeSchedule.startTimerAtFixedRate(new TemperatureUpdate(currentTemperature), Duration.ofSeconds(10));
     }
 
 
     @Override
-    public Receive<TemperatureUpdateCommand> createReceive() {
+    public Receive<TemperatureEnvironmentCommand> createReceive() {
         return newReceiveBuilder()
                 .onMessage(TemperatureUpdate.class, this::onTemperatureUpdate)
+                .onMessage(ReceiveTemperatureRequest.class, this::onTemperatureRequest)
                 .build();
     }
 
-    private Behavior<TemperatureUpdateCommand> onTemperatureUpdate(TemperatureUpdate t) {
-        getContext().getLog().info("Current temperature is: " + t.currentTemp);
+    private Behavior<TemperatureEnvironmentCommand> onTemperatureUpdate(TemperatureUpdate t) {
 
         //calculate temperature difference
-        Random random = new Random();
         double temperatureChange = (random.nextInt(62) - 31) / (double) 10;
-        getContext().getLog().info("Temperature changed by: " + temperatureChange + t.currentTemp.unit());
 
-        //calc new Temperature
-        Temperature newTemperature = null;
-        newTemperature = new Temperature(t.currentTemp.unit(), t.currentTemp.value() + temperatureChange);
         if (t.currentTemp.value() + temperatureChange > 25 || t.currentTemp.value() + temperatureChange < 15) {
-            newTemperature = new Temperature(t.currentTemp.unit(), t.currentTemp.value() + temperatureChange);
+            currentTemperature = new Temperature(t.currentTemp.unit(), t.currentTemp.value() + temperatureChange);
         } else {
-            newTemperature = new Temperature(t.currentTemp.unit(), t.currentTemp.value() - temperatureChange);
+            currentTemperature = new Temperature(t.currentTemp.unit(), t.currentTemp.value() - temperatureChange);
         }
 
         //set new Temperature
-        getContext().getLog().info("New temperature is: " + newTemperature);
-        currentTemperature = newTemperature;
+        getContext().getLog().info("[ENVIRONMENT] New temperature is: " + currentTemperature + " (changed by " +  temperatureChange + " " + t.currentTemp.unit() + ")");
+        return this;
+    }
+
+    private Behavior<TemperatureEnvironmentCommand> onTemperatureRequest(ReceiveTemperatureRequest req) {
+        req.sensor.tell(new TemperatureSensor.ReceiveTemperatureResponse(new Temperature(currentTemperature.unit(), currentTemperature.value())));
         return this;
     }
 }
--- Lab2Template/src/main/java/at/fhv/sysarch/lab2/homeautomation/ui/UI.java
+++ Lab2Template/src/main/java/at/fhv/sysarch/lab2/homeautomation/ui/UI.java
@@ -1,30 +1,27 @@
 package at.fhv.sysarch.lab2.homeautomation.ui;
 
 import akka.actor.typed.ActorRef;
-import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.PostStop;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
-import at.fhv.sysarch.lab2.homeautomation.HomeAutomationController;
 import at.fhv.sysarch.lab2.homeautomation.devices.AirCondition;
 import at.fhv.sysarch.lab2.homeautomation.devices.TemperatureSensor;
 
-import java.util.Optional;
 import java.util.Scanner;
 
 public class UI extends AbstractBehavior<Void> {
 
-    private ActorRef<TemperatureSensor.TemperatureCommand> tempSensor;
+    private ActorRef<TemperatureSensor.TemperatureSensorCommand> tempSensor;
     private ActorRef<AirCondition.AirConditionCommand> airCondition;
 
-    public static Behavior<Void> create(ActorRef<TemperatureSensor.TemperatureCommand> tempSensor, ActorRef<AirCondition.AirConditionCommand> airCondition) {
+    public static Behavior<Void> create(ActorRef<TemperatureSensor.TemperatureSensorCommand> tempSensor, ActorRef<AirCondition.AirConditionCommand> airCondition) {
         return Behaviors.setup(context -> new UI(context, tempSensor, airCondition));
     }
 
-    private  UI(ActorContext<Void> context, ActorRef<TemperatureSensor.TemperatureCommand> tempSensor, ActorRef<AirCondition.AirConditionCommand> airCondition) {
+    private  UI(ActorContext<Void> context, ActorRef<TemperatureSensor.TemperatureSensorCommand> tempSensor, ActorRef<AirCondition.AirConditionCommand> airCondition) {
         super(context);
         // TODO: implement actor and behavior as needed
         // TODO: move UI initialization to appropriate place
@@ -57,10 +54,10 @@ public class UI extends AbstractBehavior<Void> {
             // TODO: change input handling
             String[] command = reader.split(" ");
             if(command[0].equals("t")) {
-                this.tempSensor.tell(new TemperatureSensor.ReadTemperature(Optional.of(Double.valueOf(command[1]))));
+      //          this.tempSensor.tell(new TemperatureSensor.ReadTemperature(Optional.of(Double.valueOf(command[1]))));
             }
             if(command[0].equals("a")) {
-                this.airCondition.tell(new AirCondition.PowerAirCondition(Optional.of(Boolean.valueOf(command[1]))));
+        //        this.airCondition.tell(new AirCondition.PowerAirCondition(Optional.of(Boolean.valueOf(command[1]))));
             }
             // TODO: process Input
         }
