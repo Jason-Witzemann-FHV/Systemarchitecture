2503354edf63cf5ef83c443c170d087fbf1f42bd
Jason Witzemann <jason.witzemann@students.fhv.at>
2022-05-13 09:14:47 +0200

    added changes

--- Lab3Template/src/main/java/at/fhv/sysarch/lab3/pipeline/PushPipelineFactory.java
+++ Lab3Template/src/main/java/at/fhv/sysarch/lab3/pipeline/PushPipelineFactory.java
@@ -1,12 +1,30 @@
 package at.fhv.sysarch.lab3.pipeline;
 
 import at.fhv.sysarch.lab3.animation.AnimationRenderer;
+import at.fhv.sysarch.lab3.obj.Face;
 import at.fhv.sysarch.lab3.obj.Model;
+import at.fhv.sysarch.lab3.pipeline.data.Pair;
 import javafx.animation.AnimationTimer;
+import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class PushPipelineFactory {
     public static AnimationTimer createPipeline(PipelineData pd) {
         // TODO: push from the source (model)
+
+        // TODO: the connection of filters and pipes requires a lot of boilerplate code. Think about options how this can be minimized
+        IFilter source = new ModelSource<Model>();
+        IFilter filter = new ModelFilter();
+        IFilter sink = new Renderer(pd.getGraphicsContext(), pd.getRenderingMode());
+        Pipe pipe = new Pipe();
+        pipe.setSuccessor(filter);
+        source.setPipeSuccessor(pipe);
+
+        Pipe toSink = new Pipe();
+        toSink.setSuccessor(sink);
+        filter.setPipeSuccessor(toSink);
 
         // TODO 1. perform model-view transformation from model to VIEW SPACE coordinates
 
@@ -19,7 +37,7 @@ public class PushPipelineFactory {
         // lighting can be switched on/off
         if (pd.isPerformLighting()) {
             // 4a. TODO perform lighting in VIEW SPACE
-            
+
             // 5. TODO perform projection transformation on VIEW SPACE coordinates
         } else {
             // 5. TODO perform projection transformation
@@ -29,15 +47,17 @@ public class PushPipelineFactory {
 
         // TODO 7. feed into the sink (renderer)
 
+
         // returning an animation renderer which handles clearing of the
         // viewport and computation of the praction
         return new AnimationRenderer(pd) {
             // TODO rotation variable goes in here
+            private int pos = 0;
 
             /** This method is called for every frame from the JavaFX Animation
-             * system (using an AnimationTimer, see AnimationRenderer). 
+             * system (using an AnimationTimer, see AnimationRenderer).
              * @param fraction the time which has passed since the last render call in a fraction of a second
-             * @param model    the model to render 
+             * @param model    the model to render
              */
             @Override
             protected void render(float fraction, Model model) {
@@ -50,6 +70,13 @@ public class PushPipelineFactory {
                 // TODO update model-view filter
 
                 // TODO trigger rendering of the pipeline
+
+                // line
+                pd.getGraphicsContext().setStroke(Color.PINK);
+                pd.getGraphicsContext().strokeLine(0+pos,0+pos,100+pos,100+pos);
+                pos++;
+
+                source.write(model);
             }
         };
     }
